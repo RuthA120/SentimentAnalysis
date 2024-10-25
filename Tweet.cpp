91407e8d60573b4d4ad86d3e63dac3a87b548432
RuthA120 <rassefa@smu.edu>
2024-10-25 14:13:54 -0500

    Added training dataset and updated tweet class

--- Tweet.cpp
+++ Tweet.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 class Tweet{
     private:
-        string text;
-        //hello
-
-
+        string text; //holds text of tweet
+        int sentiment_score; //holds score recorded for the tweet
+        string sentiment_class; 
+    public:
+        void setText(string text){
+            this->text=text;
+        }
+        void setSentimentScore(int sentiment_score){
+            this->sentiment_score=sentiment_score;
+        }
+        int getSentimentScore(){
+            return sentiment_score;
+        }
+        void setSentimentClass(string sentiment_class){
+            this->sentiment_class=sentiment_class;
+        }
+        string getText(){
+            return text;
+        }
 };
